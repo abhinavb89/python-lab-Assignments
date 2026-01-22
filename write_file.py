ddcd222264d18f2a7dabcd4d1764e40533bb90da
abhinavb89 <80707554+abhinavb89@users.noreply.github.com>
2026-01-22 13:50:04 +0530

    Create write_file.py

---
+++ write_file.py
@@ -0,0 +1,6 @@
+file = open("sample.txt", "w")
+file.write("Hello, this file is created using Python.\n")
+file.write("This is a file writing example.")
+file.close()
+
+print("Data written to file successfully.")
